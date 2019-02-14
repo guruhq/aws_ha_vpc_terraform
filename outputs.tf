64737d06128f7dfd919448fd6e536db8bc3c815b
Karl Girthofer <karlgirthofer@gmail.com>
2019-02-14 14:10:30 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 output "vpc_id" {
   value = "${aws_vpc.main.id}"
 }
+
+output "private_route_a_id" {
+  value = "${aws_route_table.private_a.id}"
+}
+
+output "private_route_b_id" {
+  value = "${aws_route_table.private_b.id}"
+}
+
+output "private_route_c_id" {
+  value = "${aws_route_table.private_c.id}"
+}
+
+output "public_route_id" {
+  value = "${aws_route_table.public.id}"
+}
