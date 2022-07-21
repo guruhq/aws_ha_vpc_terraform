89843829503fb0c6caec881ccebbf5319b45382f
Shawn Catanzarite <me@shawncatz.com>
2022-07-21 11:14:20 -0700

    support setting tags for subnets

--- subnets.tf
+++ subnets.tf
@@ -8,30 +8,30 @@ resource "aws_subnet" "public_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet_a_cidr
   availability_zone = "${data.aws_region.current.name}a"
-  tags = {
+  tags = merge(var.public_subnet_tags, {
     Name = "${var.vpc_name}-public-a"
     Tier = "Public"
-  }
+  })
 }
 
 resource "aws_subnet" "public_b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet_b_cidr
   availability_zone = "${data.aws_region.current.name}b"
-  tags = {
+  tags = merge(var.public_subnet_tags, {
     Name = "${var.vpc_name}-public-b"
     Tier = "Public"
-  }
+  })
 }
 
 resource "aws_subnet" "public_c" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet_c_cidr
   availability_zone = "${data.aws_region.current.name}c"
-  tags = {
+  tags = merge(var.public_subnet_tags, {
     Name = "${var.vpc_name}-public-c"
     Tier = "Public"
-  }
+  })
 }
 
 /*
@@ -42,28 +42,28 @@ resource "aws_subnet" "private_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_a_cidr
   availability_zone = "${data.aws_region.current.name}a"
-  tags = {
+  tags = merge(var.private_subnet_tags, {
     Name = "${var.vpc_name}-private-a"
     Tier = "Private"
-  }
+  })
 }
 
 resource "aws_subnet" "private_b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_b_cidr
   availability_zone = "${data.aws_region.current.name}b"
-  tags = {
+  tags = merge(var.private_subnet_tags, {
     Name = "${var.vpc_name}-private-b"
     Tier = "Private"
-  }
+  })
 }
 
 resource "aws_subnet" "private_c" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_c_cidr
   availability_zone = "${data.aws_region.current.name}c"
-  tags = {
+  tags = merge(var.private_subnet_tags, {
     Name = "${var.vpc_name}-private-c"
     Tier = "Private"
-  }
+  })
 }
